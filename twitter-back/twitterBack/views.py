40500594bbb2087bc67e66b507ae43772f6acd1d
Karen Moraes <karenmoraes222@gmail.com>
2025-10-16 17:53:08 -0300

    finalizado

--- twitter-back/twitterBack/views.py
+++ twitter-back/twitterBack/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 @api_view(['POST'])
 def register_user(request):
@@ -10,12 +11,16 @@ def register_user(request):
     email = request.data.get('email')
     password = request.data.get('password')
 
+    if not username or not password:
+        return Response({'error': 'Usuário e senha são obrigatórios'}, status=status.HTTP_400_BAD_REQUEST)
+
     if User.objects.filter(username=username).exists():
-        return Response({'error': 'Usuario ja existe'})
+        return Response({'error': 'Usuário já existe'}, status=status.HTTP_400_BAD_REQUEST)
 
     user = User.objects.create_user(username=username, password=password, email=email)
     token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key})
+    return Response({'token': token.key, 'user_id': user.id}, status=status.HTTP_201_CREATED)
+
 
 @api_view(['POST'])
 def login_user(request):
@@ -25,7 +30,6 @@ def login_user(request):
     user = authenticate(username=username, password=password)
     if user is not None:
         token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'user_id': user.id})
+        return Response({'token': token.key, 'user_id': user.id}, status=status.HTTP_200_OK)
     else:
-        return Response({'error': 'Credenciais invalidas'})
-
+        return Response({'error': 'Credenciais inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
